dcfdcb502ecfac8061a6aa7bba17861686422a24
INTRACTO - Jeroen Tubex <jeroen.tubex@intracto.com>
2018-11-08 13:30:32 +0100

    Add social media count

--- src/Plugin/Block/SocialMediaBlock.php
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -22,10 +22,15 @@ class SocialMediaBlock extends BlockBase {
       '#theme' => 'social-media',
       '#attached' => ['library' => ['thomas_more_social_media/social_media']],
       '#facebook_url' => \Drupal::state()->get('thomas_more_social_media.facebook_url'),
+      '#facebook_count' => 2,
       '#google_plus_url' => \Drupal::state()->get('thomas_more_social_media.google_plus_url'),
+      '#google_plus_count' => 5,
       '#twitter_url' => \Drupal::state()->get('thomas_more_social_media.twitter_url'),
+      '#twitter_count' => 3,
       '#linkedin_url' => \Drupal::state()->get('thomas_more_social_media.linkedin_url'),
+      '#linkedin_count' => 7,
       '#foursquare_url' => \Drupal::state()->get('thomas_more_social_media.foursquare_url'),
+      '#foursquare_count' => 2,
     ];
   }
 
