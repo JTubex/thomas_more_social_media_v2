a1da6879e9d6e4f513308ccdccca846e7007aacb
INTRACTO - Jeroen Tubex <jeroen.tubex@intracto.com>
2018-11-08 15:24:36 +0100

    Add DI

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -3,14 +3,28 @@
 namespace Drupal\thomas_more_social_media\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AjaxController extends ControllerBase {
 
+  protected $database;
+
+  public function __construct(Connection $database) {
+    $this->database = $database;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
   public function counter(Request $request) {
     if (!$this->currentUser()->hasPermission('skip tracking clicks')) {
-      \Drupal::database()->insert('thomas_more_social_media_counter')
+      $this->database->insert('thomas_more_social_media_counter')
         ->fields([
           'network' => $request->get('network'),
           'time_clicked' => \Drupal::time()->getRequestTime(),
--- src/Plugin/Block/SocialMediaBlock.php
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -3,6 +3,10 @@
 namespace Drupal\thomas_more_social_media\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\State\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a social menu block.
@@ -12,7 +16,28 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("Social media"),
  * )
  */
-class SocialMediaBlock extends BlockBase {
+class SocialMediaBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  protected $database;
+
+  protected $state;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $database, StateInterface $state) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->database = $database;
+    $this->state = $state;
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('database'),
+      $container->get('state')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -21,21 +46,21 @@ class SocialMediaBlock extends BlockBase {
     return [
       '#theme' => 'social-media',
       '#attached' => ['library' => ['thomas_more_social_media/social_media']],
-      '#facebook_url' => \Drupal::state()->get('thomas_more_social_media.facebook_url'),
+      '#facebook_url' => $this->state->get('thomas_more_social_media.facebook_url'),
       '#facebook_count' => $this->getSocialMediaCount('facebook'),
-      '#google_plus_url' => \Drupal::state()->get('thomas_more_social_media.google_plus_url'),
+      '#google_plus_url' => $this->state->get('thomas_more_social_media.google_plus_url'),
       '#google_plus_count' => $this->getSocialMediaCount('google_plus'),
-      '#twitter_url' => \Drupal::state()->get('thomas_more_social_media.twitter_url'),
+      '#twitter_url' => $this->state->get('thomas_more_social_media.twitter_url'),
       '#twitter_count' => $this->getSocialMediaCount('twitter'),
-      '#linkedin_url' => \Drupal::state()->get('thomas_more_social_media.linkedin_url'),
+      '#linkedin_url' => $this->state->get('thomas_more_social_media.linkedin_url'),
       '#linkedin_count' => $this->getSocialMediaCount('linkedin'),
-      '#foursquare_url' => \Drupal::state()->get('thomas_more_social_media.foursquare_url'),
+      '#foursquare_url' => $this->state->get('thomas_more_social_media.foursquare_url'),
       '#foursquare_count' => $this->getSocialMediaCount('foursquare'),
     ];
   }
 
   protected function getSocialMediaCount($network) {
-    $query = \Drupal::database()->select('thomas_more_social_media_counter', 't');
+    $query = $this->database->select('thomas_more_social_media_counter', 't');
     $query->condition('t.network', $network);
     return $query->countQuery()->execute()->fetchField();
   }
