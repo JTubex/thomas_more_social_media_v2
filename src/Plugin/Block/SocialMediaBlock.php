04bf2c51eceb68bf265d037d94d35d9b92670140
INTRACTO - Jeroen Tubex <jeroen.tubex@intracto.com>
2018-11-08 13:48:41 +0100

    Add social media count queries

--- src/Plugin/Block/SocialMediaBlock.php
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -22,16 +22,22 @@ class SocialMediaBlock extends BlockBase {
       '#theme' => 'social-media',
       '#attached' => ['library' => ['thomas_more_social_media/social_media']],
       '#facebook_url' => \Drupal::state()->get('thomas_more_social_media.facebook_url'),
-      '#facebook_count' => 2,
+      '#facebook_count' => $this->getSocialMediaCount('facebook'),
       '#google_plus_url' => \Drupal::state()->get('thomas_more_social_media.google_plus_url'),
-      '#google_plus_count' => 5,
+      '#google_plus_count' => $this->getSocialMediaCount('google_plus'),
       '#twitter_url' => \Drupal::state()->get('thomas_more_social_media.twitter_url'),
-      '#twitter_count' => 3,
+      '#twitter_count' => $this->getSocialMediaCount('twitter'),
       '#linkedin_url' => \Drupal::state()->get('thomas_more_social_media.linkedin_url'),
-      '#linkedin_count' => 7,
+      '#linkedin_count' => $this->getSocialMediaCount('linkedin'),
       '#foursquare_url' => \Drupal::state()->get('thomas_more_social_media.foursquare_url'),
-      '#foursquare_count' => 2,
+      '#foursquare_count' => $this->getSocialMediaCount('foursquare'),
     ];
+  }
+
+  protected function getSocialMediaCount($network) {
+    $query = \Drupal::database()->select('thomas_more_social_media_counter', 't');
+    $query->condition('t.network', $network);
+    return $query->countQuery()->execute()->fetchField();
   }
 
 }
