4d0ca88d8b70dd1b462dbb50df363492e7ae90f2
INTRACTO - Jeroen Tubex <jeroen.tubex@intracto.com>
2018-11-08 14:31:51 +0100

    Add ajax controller

---
+++ src/Controller/AjaxController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\thomas_more_social_media\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AjaxController extends ControllerBase {
+
+  public function counter(Request $request) {
+    \Drupal::database()->insert('thomas_more_social_media_counter')
+      ->fields([
+        'network' => $request->get('network'),
+        'time_clicked' => \Drupal::time()->getRequestTime(),
+      ])->execute()
+    ;
+    return new Response('Ok');
+  }
+
+}
