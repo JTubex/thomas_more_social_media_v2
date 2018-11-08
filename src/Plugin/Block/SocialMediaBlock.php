edae089186ca1e2ad808ea737b66fd61978750cb
INTRACTO - Jeroen Tubex <jeroen.tubex@intracto.com>
2018-11-08 10:59:47 +0100

    Add block structure

---
+++ src/Plugin/Block/SocialMediaBlock.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\thomas_more_social_media\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Defines a social menu block.
+ *
+ * @Block(
+ *  id = "thomas_more_social_media_block",
+ *  admin_label = @Translation("Social media"),
+ * )
+ */
+class SocialMediaBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    
+  }
+
+}
